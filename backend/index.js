abbf9e2ccf92c2adb54adb11f8b2da9d60721283
william nyarash <114805151+William-nyarash@users.noreply.github.com>
2024-08-08 13:26:11 +0300

    add environmental variables

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+require('dotenv').config()
 const mysql = require('mysql')
 const app = express()
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"waren2node",
-    database:"library"
+    host: process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
 })
+app.use(express.json())
 app.get('/home',(req,res)=>{
     res.json("hello world");
 })
@@ -23,16 +25,20 @@ app.get('/books',(req,res)=>{
 })
 
 app.post('/book',(res,req)=>{
-
+    
+    const body = res.body
+    console.log(body)
     const q = "INSERT INTO books (`title`,`description`,`cover`) VALUES  (?)"
-    const values = [
-       "title from bakend",
-       "description from bakend",
-       "description.png"
-    ];
+    const values = ({
+       title : body.title,
+       description :body.description,
+       cover: body.cover
+     });
+
     db.query(q,[values],(err,data)=>{
         if(err) return res.json(err)
         else 
+            console.log(data)
             return res.json(data)
     })
 })
